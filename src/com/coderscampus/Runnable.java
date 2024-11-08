208a21c2d67156ff436f7fcb63757781413455a3
Yehoshua Holstein <holsteinyehoshua@gmail.com>
2024-11-07 20:42:51 -0500

    Final pre-submission ommit

--- src/com/coderscampus/Runnable.java
+++ src/com/coderscampus/Runnable.java
@@ -19,26 +19,11 @@ public class Runnable {
 		College[] course2List = collegeServicecontainer.getCourse2();
 		College[] course3List = collegeServicecontainer.getCourse3();
 
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter("course1.csv"))) {
-			writer.write("Student ID" + "," + "Student Name" + "," + "Course" + " ," + "Grade\n");
-			for (College result1 : course1List) {
-				writer.write(result1.getStudentID() + "," + result1.getStudentName() + "," + result1.getCourse() + ","
-						+ result1.getGrade() + "\n");
-			}
-		}
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter("course2.csv"))) {
-			writer.write("Student ID" + "," + "Student Name" + "," + "Course" + " ," + "Grade\n");
-			for (College result2 : course2List) {
-				writer.write(result2.getStudentID() + "," + result2.getStudentName() + "," + result2.getCourse() + ","
-						+ result2.getGrade() + "\n");
-			}
-		}
-		try (BufferedWriter writer = new BufferedWriter(new FileWriter("course3.csv"))) {
-			writer.write("Student ID" + "," + "Student Name" + "," + "Course" + " ," + "Grade\n");
-			for (College result3 : course3List) {
-				writer.write(result3.getStudentID() + "," + result3.getStudentName() + "," + result3.getCourse() + ","
-						+ result3.getGrade() + "\n");
-			}
-		}
+		FileServiceWriter fileServiceWriter = new FileServiceWriter();
+
+		fileServiceWriter.CreateFile("course1", course1List);
+		fileServiceWriter.CreateFile("course2", course2List);
+		fileServiceWriter.CreateFile("course3", course3List);
+
 	}
 }
